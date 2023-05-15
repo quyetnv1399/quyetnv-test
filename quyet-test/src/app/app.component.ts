cb55991491b629c0e9daa65e1182f44f27e426bb
quyetnv <quyetnv.tci@gmail.com>
2023-05-15 23:39:27 +0700

    setup translate

--- quyet-test/src/app/app.component.ts
+++ quyet-test/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quyet-test';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('vn');
+    translate.use('vn');
+  }
 }
